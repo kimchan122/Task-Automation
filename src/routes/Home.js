9fab51cf6ead8c6afdbe5cfff92de464ede3877b
kimchan122 <chanchankim122@gmail.com>
2024-06-23 19:26:45 +0700

    fixed homepage url

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home page!</h1>
-      {/* Add your content here */}
     </div>
   );
 };
