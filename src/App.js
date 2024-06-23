4fb5f42970d79f1759fa0ac1b0aa943603e731cb
kimchan122 <chanchankim122@gmail.com>
2024-06-23 19:41:23 +0700

    fixed url

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./routes/Home";
 import PrivacyPolicy from "./routes/PrivacyPolicy";
 import TermsOfService from "./routes/TermsOfService";
+import NotFound from "./routes/NotFound";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Task-Automation">
       <div>
         <Link to="/">Home</Link>
         <Link to="/privacypolicy">Privacy Policy</Link>
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/privacypolicy" element={<PrivacyPolicy />} />
         <Route path="/termsofservice" element={<TermsOfService />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
